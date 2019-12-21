c6d2de7c53525204ebf4eeb64297ab1df2ede1a1
eandy5000 <eandy5000@yahoo.com>
2019-12-21 05:14:09 -0600

    rebuilt for practice

--- lib/candidate.ex
+++ lib/candidate.ex
@@ -1,18 +1,5 @@
 defmodule Candidate do
-  defstruct [:id, :name, votes: 0]
-
-  @doc """
-  new method creates a new candidata
-
-  ## Parameters
-    - id: candidate id
-    - name: name of the candidate
-
-  ## Example
-    iex> Candidate.new(1, "Joe Smith")
-    %Candidate{id: 1, name: "Joe Smith", votes: 0}
-
-  """
+  defstruct [:name, :id, votes: 0]
 
   def new(id, name) do
     %Candidate{id: id, name: name}
--- lib/election.ex
+++ lib/election.ex
@@ -3,24 +3,20 @@ defmodule Election do
     name: "Dog Catcher",
     candidates: [
       Candidate.new(1, "Joe Smith"),
-      Candidate.new(2, "Bill Jones")
+      Candidate.new(2, "Bill Davis")
     ],
     next_id: 3
   )
 
   def run() do
-    %Election{}
-    |> run()
+    %Election{} |> run()
   end
 
   def run(:quit), do: :quit
 
   def run(election = %Election{}) do
     [
-      clear_hack(),
-      clear_hack(),
-      clear_hack(),
-      clear_hack()
+      hack_clear()
     ]
     |> IO.write()
 
@@ -28,7 +24,7 @@ defmodule Election do
     |> view()
     |> IO.write()
 
-    cmd = IO.gets(">")
+    cmd = IO.gets(" > ")
 
     election
     |> update(cmd)
@@ -36,8 +32,10 @@ defmodule Election do
   end
 
   def update(election, cmd) when is_binary(cmd) do
-    update(election, String.split(cmd))
+    update(election, String.split(cmd))  
   end
+
+  def update(_election, ["q" <> _ | _args]), do: :quit
 
   def update(election, ["n" <> _ | args]) do
     name = Enum.join(args, " ")
@@ -48,24 +46,27 @@ defmodule Election do
     name = Enum.join(args, " ")
     candidate = Candidate.new(election.next_id, name)
     candidates = [candidate | election.candidates]
-    %{election | candidates: candidates, next_id: election.next_id + 1}
+
+    # %{election | candidates: candidates, next_id: election.next_id}
+    election
+    |> Map.put(:candidates, candidates)
+    |> Map.put(:next_id, election.next_id + 1)
   end
 
-  def update(_election, ["q" <> _]), do: :quit
 
-  def update(election, ["v" <> _, id]) do
+  def update(election, ["v" <> _ , id]) do
     vote(election, Integer.parse(id))
   end
 
-  defp vote(election, {id, ""}) do
-    candidates =
-      election.candidates
-      |> Enum.map(&maybe_inc_votes(&1, id))
+  def update(election, _errors), do: election
 
+
+  def vote(election, {id, ""}) do
+    candidates = Enum.map(election.candidates, &maybe_inc_vote(&1, id))
     %{election | candidates: candidates}
   end
 
-  defp vote(election, _errors), do: election
+  def vote(election, _errors), do: election
 
   def view(election) do
     [
@@ -75,57 +76,59 @@ defmodule Election do
     ]
   end
 
-  def view_body(election) do
+  defp view_body(election) do
     election.candidates
     |> sort_by_votes_desc()
     |> candidates_to_string()
     |> prepend_header_to_candidates()
   end
 
-  def view_header(election) do
+  defp view_header(election) do
     [
       "Election for: #{election.name}\n"
     ]
   end
 
-  def view_footer() do
+  defp view_footer() do
     [
       "\n",
-      "commands: (n)ame <election>, (a)dd <candidate>, (v)ote <id>, (q)uit\n"
+      "(n)ame <election> (a)dd <candidate> (v)ote <id> (q)uit"
     ]
   end
 
-  def sort_by_votes_desc(candidates) do
+  defp maybe_inc_vote(candidate, id) when is_integer(id) do
+    maybe_inc_vote(candidate, candidate.id == id)
+  end
+
+  defp maybe_inc_vote(candidate, _inc_vote = false), do: candidate
+
+  defp maybe_inc_vote(candidate, _inc_vote = true) do
+    candidate
+    |> Map.update!(:votes, &(&1 + 1))
+  end
+
+  defp candidates_to_string(candidates) do
     candidates
-    |> Enum.sort(&(&1.votes >= &2.votes))
-  end
-
-  defp maybe_inc_votes(candidate, id) when is_integer(id) do
-    maybe_inc_votes(candidate, candidate.id == id)
-  end
-
-  defp maybe_inc_votes(candidate, _inc_vote = false), do: candidate
-
-  defp maybe_inc_votes(candidate, _inc_vote = true) do
-    Map.update!(candidate, :votes, &(&1 + 1))
-  end
-
-  def prepend_header_to_candidates(candidates) do
-    [
-      "ID\tVotes\tName\n",
-      "--------------------------------\n"
-      | candidates
-    ]
-  end
-
-  def candidates_to_string(candidates) do
-    candidates
-    |> Enum.map(fn %{id: id, name: name, votes: votes} ->
+    |> Enum.map(fn %{id: id, name: name, votes: votes} -> 
       "#{id}\t#{votes}\t#{name}\n"
     end)
   end
 
-  def clear_hack() do
-    "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+  defp sort_by_votes_desc(candidates) do
+    candidates
+    |> Enum.sort(&(&1.votes >= &2.votes))
   end
+  
+  defp prepend_header_to_candidates(candidates) do
+    [
+      "ID\tVOTES\tNAME\n",
+      "-------------------------------------------\n"
+      | candidates
+    ]
+  end
+
+  defp hack_clear() do
+    "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+  end
+
 end
